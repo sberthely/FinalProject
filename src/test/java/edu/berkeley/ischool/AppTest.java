5f059f66e21fae8350084c71103735d2c8723a24
sberthely <sberthely@berkeley.edu>
2017-04-07 13:01:40 -0700

    Initial commit

---
+++ src/test/java/edu/berkeley/ischool/AppTest.java
@@ -0,0 +1,13 @@
+package edu.berkeley.ischool;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest
+{
+    @org.junit.Test
+    public void shouldWork() {
+
+    }
+}
