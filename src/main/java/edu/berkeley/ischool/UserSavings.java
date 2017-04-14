ebc9444b70abac5052ec4e86f56ddef672ada868
sberthely <sberthely@berkeley.edu>
2017-04-13 21:39:13 -0700

    Change the first test - Delete any attempt to read a file and Create a user profile and update paid_installments. Test a failing test by trying to update a larger amount of paid_installments than possible number_installments.

--- src/main/java/edu/berkeley/ischool/UserSavings.java
+++ src/main/java/edu/berkeley/ischool/UserSavings.java
@@ -1,19 +1,11 @@
-
-
 package edu.berkeley.ischool;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 //Ask input from the user regarding interest rate and installments.
 public class UserSavings
 {
-    final static Charset ENCODING = StandardCharsets.UTF_8;
     private String name;
     private Double monthly_payment;
     private Double interest_rate;
@@ -27,14 +19,6 @@ public class UserSavings
         this.num_installments = num_installments;
     }
 
-    public String readFromTextFile(String aFileName) throws IOException {
-        String line;
-        Path path = Paths.get(aFileName);
-        try (BufferedReader reader = Files.newBufferedReader(path, ENCODING)) {
-            line = reader.readLine();
-        }
-        return line;
-    }
 
     public boolean updatePaidInstallments(int paid_installments){
         if (this.num_installments < paid_installments) return false;
--- src/test/java/edu/berkeley/ischool/UserSavingsTest.java
+++ src/test/java/edu/berkeley/ischool/UserSavingsTest.java
@@ -3,19 +3,15 @@ package edu.berkeley.ischool;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 
 public class UserSavingsTest {
 
-    private String FILE_NAME;
     UserSavings userSavings;
 
     @Before
     public void initObjects(){
-        FILE_NAME = "/Users/SelBerthely/IdeaProjects/FinalProject/src/output.txt";
         userSavings = new UserSavings("Selenne", 2000.0, 5.0, 10);
 
     }
