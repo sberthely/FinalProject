2311632ee6348a3ac25121ae7661d5f4abadeeef
sberthely <sberthely@berkeley.edu>
2017-04-13 21:49:35 -0700

    Test 2 - Calculate the monthly_interest_rate based on each user annual_interest_rate --> calculateMonthlyInterestRate()

--- src/main/java/edu/berkeley/ischool/UserSavings.java
+++ src/main/java/edu/berkeley/ischool/UserSavings.java
@@ -1,28 +1,29 @@
 package edu.berkeley.ischool;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
 //Ask input from the user regarding interest rate and installments.
 public class UserSavings
 {
     private String name;
     private Double monthly_payment;
-    private Double interest_rate;
+    private Double annual_interest_rate;
     private int num_installments;
     protected int paid_installments;
+    final static int YEAR_MONTHLY_INSTALLMENTS = 12;
 
-    public UserSavings(String name, Double monthly_payment, Double interest_rate, int num_installments){
+    public UserSavings(String name, Double monthly_payment, Double annual_interest_rate, int num_installments){
         this.name = name;
         this.monthly_payment = monthly_payment;
-        this.interest_rate = interest_rate;
+        this.annual_interest_rate = annual_interest_rate;
         this.num_installments = num_installments;
     }
-
 
     public boolean updatePaidInstallments(int paid_installments){
         if (this.num_installments < paid_installments) return false;
         this.paid_installments = paid_installments;
         return true;
+    }
+
+    public Double calculateMonthlyInterestRate() {
+        return Math.round(annual_interest_rate / YEAR_MONTHLY_INSTALLMENTS * 100) / 100.00;
     }
 }
--- src/test/java/edu/berkeley/ischool/UserSavingsTest.java
+++ src/test/java/edu/berkeley/ischool/UserSavingsTest.java
@@ -8,22 +8,46 @@ import static junit.framework.TestCase.assertFalse;
 
 public class UserSavingsTest {
 
-    UserSavings userSavings;
+    UserSavings userSavings1, userSavings2, userSavings3, userSavings4;
 
     @Before
     public void initObjects(){
-        userSavings = new UserSavings("Selenne", 2000.0, 5.0, 10);
-
+        userSavings1 = new UserSavings("Selenne", 2000.0, 12.0, 10);
+        userSavings2 = new UserSavings("Jose", 10000.0, 15.0, 20);
+        userSavings3 = new UserSavings("Ramon", 1500.0, 9.5, 20);
+        userSavings4 = new UserSavings("Ramon", 1500.0, 0.0, 20);
     }
 
+    //Test 1 - Create a userSavings class and update paid_installments.
     @Test
     public void updatePaidInstallmentsShouldWork(){
-        userSavings.updatePaidInstallments(5);
-        assertEquals(5, userSavings.paid_installments);
+        userSavings1.updatePaidInstallments(5);
+        assertEquals(5, userSavings1.paid_installments);
     }
 
     @Test
     public void updateMorePaidInstallmentsThanPossibleShouldNotWork(){
-        assertFalse(userSavings.updatePaidInstallments(15));
+        assertFalse(userSavings1.updatePaidInstallments(15));
+    }
+
+    //Test 2 - Calculate the monthly_interest_rate based on each user annual_interest_rate --> calculateMonthlyInterestRate().
+    @Test
+    public void twelveMonthlyInterestRateShouldBe1(){
+        assertEquals(1.00, userSavings1.calculateMonthlyInterestRate());
+    }
+
+    @Test
+    public void fifteenMonthlyInterestRateShouldBe1Dot25(){
+        assertEquals(1.25, userSavings2.calculateMonthlyInterestRate());
+    }
+
+    @Test
+    public void nineDotFiveMonthlyInterestRateShouldBeDot79(){
+        assertEquals(0.79, userSavings3.calculateMonthlyInterestRate());
+    }
+
+    @Test
+    public void zeroMonthlyInterestRateShouldBeZero(){
+        assertEquals(0.0, userSavings4.calculateMonthlyInterestRate());
     }
 }
